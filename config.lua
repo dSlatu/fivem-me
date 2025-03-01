c60129d4ba2135c19c7c5682345fa4706834c10a
Slatu <138379901+dSlatu@users.noreply.github.com>
2025-03-01 21:28:59 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {
+    language = 'fr',
+    color = { r = 230, g = 230, b = 230, a = 255 }, 
+    font = 0, 
+    time = 5000, 
+    scale = 0.5, 
+    dist = 250,
+}
+
+
+Languages = {
+
+    ['fr'] = {
+        commandName = 'me',
+        commandDescription = 'Affiche une action au dessus de votre tête.',
+        commandSuggestion = {{ name = 'action'}},
+        prefix = 'l\'individu '
+    },
+
+}
